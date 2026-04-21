32482a287cf194614e0ce4163369afeeb12ec605
dchlfin <167969296+dchlfin@users.noreply.github.com>
2026-04-21 23:38:34 +0400

    feat: create title label

--- currency-converter.py
+++ currency-converter.py
@@ -11,10 +11,24 @@ class CurrencyConverter(Tk):
             self.rowconfigure(i, weight = 1)
 
         # widgets
+        self.title_label = Title(self)
+        self.title_label.grid(row = 0, sticky = EW)
         # self.input_frame = InputFrame(self)
         # self.input_frame.grid(row = 0, sticky = EW, pady = (0,5))
         
         self.mainloop()
+
+class Title(Frame):
+    def __init__(self, parent):
+        super().__init__(parent, height = 50, width = 300)
+
+        # layout configuration
+        self.columnconfigure(0, weight = 1)
+        self.rowconfigure(0, weight = 1)
+        self.grid_propagate(False)
+
+        self.title = Label(self, text = "Currency Converter", font = ('Helvetica', 12), background = '#BDB6AC')
+        self.title.grid(row = 0, rowspan = 2, column = 0, columnspan = 2, sticky = NSEW)
 
 class InputFrame(Frame):
     def __init__(self, parent):
